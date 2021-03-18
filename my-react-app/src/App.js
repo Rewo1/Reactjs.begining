a17c67510a860349fd62349e67ffc108895fbcc5
rewo1 <rawinakhatiwada1998@gmail.com>
2021-03-18 15:26:07 +0545

    second commit

---
+++ my-react-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import First from './First';
+import Second from './Second';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>hello world</h1>
+      <p>we are reading react right now</p>
+      <First/> */}
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/First.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function First() {
+    return (
+        <>
+        <p>this is a react js , this is our first component buid from the react js </p>
+        </>
+    )
+}
